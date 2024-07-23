62ee3c452035ed9d6849bda402eead6fc550ccd6
Ali Ahmed Iraqui <aliiraqui@hotmail.com>
2024-07-23 21:27:21 +0530

    Created Person Class

---
+++ src/domain/Person.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Person extends AbstractEntity {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("~~~ Person ~~~\n" +
+                "Name: " + name);
+    }
+}
